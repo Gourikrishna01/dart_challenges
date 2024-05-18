1e9a5beb43e8ff6ba972ad542aae48244f132d90
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-05-18 16:07:26 +0530

    message

---
+++ main.dart
@@ -0,0 +1,6 @@
+void main() {
+  //program//
+  String name = "gouri";
+  int age = 23;
+  print("my name is  ${name} i am  ${age}");
+}
---
+++ variable_datatype.dart
@@ -0,0 +1,62 @@
+void main() {
+  //numbers//
+  int num1 = 20;
+
+  double num2 = 20.5;
+
+  num num3 = 20;
+  num num4 = 90.4;
+
+//string//
+  String course = "welcome to Flutter";
+  // print(course);
+//bool//
+  bool isSigned = false;
+  // print(isSigned);
+//list//
+  List numbers = [1];
+  List Fruits = ["apple", "orange", "lemon", "grape"];
+  // print(Fruits.first);
+  // print(Fruits.last);
+  // print(Fruits.isNotEmpty);
+  // print(Fruits.length);
+  // print(numbers.reversed);
+  // print(numbers.single);
+//list methods//
+  List number2 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+//Addmethod//
+  // number2.add(50);
+//addall//
+  // number2.addAll([60, 70, 80, 90]);
+  // print(number2);
+//insert//
+  // print(number2);
+  // number2.insert(4, 50);
+  // print(number2);
+//insertall//
+  // print(number2);
+  // number2.insertAll(4, [50, 60, 70, 80]);
+  // print(number2);
+//update//
+  // print(number2);
+  // number2[3] = 35;
+  // print(number2);
+  // print(number2);
+  // // number2.remove(30);
+  // // number2.removeAt(0);
+  // // number2.removeLast();
+  // number2.removeRange(0, 4);
+  // print(number2);
+//MAP//
+  Map Player = {
+    "name": "sachin",
+    "age": 20,
+    "place": "delhi",
+    "team": "rcb",
+    "wicket": 100
+  };
+  print(Player);
+  Player.clear();
+  print(Player);
+}
