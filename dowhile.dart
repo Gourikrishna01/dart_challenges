ae82195cf428c1be4112390dc8754e506c05334c
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-05-19 12:05:04 +0530

    message

---
+++ dowhile.dart
@@ -0,0 +1,7 @@
+void main() {
+  var x = 0;
+  do {
+    print(x);
+    x++;
+  } while (x <= 10);
+}
---
+++ forloops.dart
@@ -0,0 +1,15 @@
+void main() {
+  // for (var i = 0; i <= 10; i++) {
+  //   print(i);
+  // }
+//for in//
+  var numbers = [1, 2, 3, 4, 5, 6, 7];
+  var fruits = ["a", "b", "v"];
+  for (var x in fruits) {
+    if (x == "a") {
+      print("yes");
+    } else {
+      print("no");
+    }
+  }
+}
