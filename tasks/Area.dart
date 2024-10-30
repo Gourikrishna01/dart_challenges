2eb6a625361205fcce75fac6c8e1fe767b134f0b
Gourikrishna01 <gourimadathil2001@gmail.com>
2024-10-30 21:28:27 +0530

    message

---
+++ tasks/Area.dart
@@ -0,0 +1,6 @@
+void main() {
+  int a = 4;
+  int b = 9;
+  int area = a * b;
+  print("area of rectangle${area}");
+}
---
+++ tasks/Variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num = 2;
+  String name = "Gouri";
+  bool val = (name == num);
+  List list = [1, 2, 4, 5, 6, 7];
+  Map map = {"name": "Gouri", "lname": "Krishna", "rollno": 24};
+  print(num);
+  print(name);
+  print(val);
+  print(list);
+  print(map);
+}
